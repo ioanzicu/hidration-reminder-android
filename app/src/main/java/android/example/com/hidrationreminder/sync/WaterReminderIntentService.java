77f3619e8c5654f3bb290906c710336dc58081a2
ioanz <ioan.zicu94@gmail.com>
2020-02-04 18:26:45 +0100

    setup project files

---
+++ app/src/main/java/android/example/com/hidrationreminder/sync/WaterReminderIntentService.java
@@ -0,0 +1,11 @@
+package android.example.com.hidrationreminder.sync;
+
+// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+
+//  TODO (10) Create a default constructor that calls super with the name of this class
+
+//  TODO (11) Override onHandleIntent
+//      TODO (12) Get the action from the Intent that started this Service
+//      TODO (13)
+public class WaterReminderIntentService {
+}
